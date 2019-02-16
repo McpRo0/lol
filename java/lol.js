e794c67082e4a7dc73dd6488dbe936ae22718a35
McpRo0 <arsen.pasenyk@gmail>
2019-02-16 13:59:16 +0200

    lesson 3

---
+++ java/lol.js
@@ -0,0 +1,8 @@
+var array = ["Кошка", "Кот", "Белка"]
+array.pop();
+console.log(array);
+array.push("Кит","Слон","Яблоко", 77, true, ["String", "String2"])
+console.log(array [7][1]);
+array.shift();
+console.log(array)
+array.unshift("Кит","Слон","Яблоко");
